3756f6c85d89ddb62e699c609d105c60a61f9277
elliot-winch <erw331@nyu.edu>
2017-12-07 00:10:54 -0500

    Data miscollection bug fixed

--- TweetCollector.py
+++ TweetCollector.py
@@ -2,7 +2,7 @@ import os
 import csv
 import tweepy
 from textblob import TextBlob
-
+from datetime import datetime
 
 #Twitter API credentials
 consumer_key = "FJWOzGHGKB71vVWaARRJs43Sp"
@@ -24,68 +24,79 @@ class TweetSemanticAnalyser():
     def getAllUserTweets(self, createFile=True):
         #Twitter only allows access to a users most recent 3240 tweets with this method
 
-        #authorize twitter, initialize tweepy
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_key, access_secret)
-        api = tweepy.API(auth)
-
-        #make initial request for most recent tweets (200 is the maximum allowed count)
-        new_tweets = api.user_timeline(self.username,count=200)
-
-        #save most recent tweets
-        self.alltweets.extend(new_tweets)
-
-        if(len(self.alltweets) <= 0):
-            print("Failed to find tweets")
-            return
-
-        #save the id of the oldest tweet less one
-        oldest = self.alltweets[-1].id - 1
-
-        #keep grabbing tweets until there are no tweets left to grab
-        while len(new_tweets) > 0:
-            print ("getting tweets for %s" % (self.username))
-
-            #all subsiquent requests use the max_id param to prevent duplicates
-            new_tweets = api.user_timeline(screen_name = self.username,count=200,max_id=oldest)
-
+        try:
+            with open(username + "_tweets.csv", "r") as f: 
+                print(username + " tweets found")
+                csvreader = csv.reader(f, delimiter = ",")
+                for row in csvreader:
+                    try:
+                        datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S")
+                        
+                        self.semanticScores[row[1]] = TextBlob(row[2]).sentiment
+                    except IndexError:
+                        continue
+                    except ValueError:
+                        continue
+                                
+        except FileNotFoundError:
+            #authorize twitter, initialize tweepy
+            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_key, access_secret)
+            api = tweepy.API(auth)
+    
+            #make initial request for most recent tweets (200 is the maximum allowed count)
+            new_tweets = api.user_timeline(self.username,count=200)
+    
             #save most recent tweets
             self.alltweets.extend(new_tweets)
-
-            #update the id of the oldest tweet less one
+    
+            if(len(self.alltweets) <= 0):
+                print("Failed to find tweets")
+                return
+    
+            #save the id of the oldest tweet less one
             oldest = self.alltweets[-1].id - 1
-            print ("...%s tweets downloaded so far" % (len(self.alltweets)))
-
-        #transform the tweepy tweets into a 2D array that will populate the csv
-        outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in self.alltweets]
-        location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-        if(createFile):
-            with open(os.path.join(location, '%s_tweets.csv' % self.username), 'w+') as f:
-                writer = csv.writer(f)
-                writer.writerow(["id","created_at","text"])
-                writer.writerows(outtweets)
-        
-        if(self.filtered):
-            for tweet in self.alltweets:
-                self.semanticScores[tweet.created_at] = TextBlob(tweet.text).sentiment
-        else:
-            for tweet in self.alltweets:
-                tweetByWords = tweet.text.split()
-                
-                
-                for word in tweetByWords:
-                    for filterWord in self.filterWords:
-                        if(word == filterWord):
-                            self.semanticScores[tweet.created_at] = TextBlob(tweet.text).sentiment
+    
+            #keep grabbing tweets until there are no tweets left to grab
+            while len(new_tweets) > 0:
+                print ("getting tweets for %s" % (self.username))
+    
+                #all subsiquent requests use the max_id param to prevent duplicates
+                new_tweets = api.user_timeline(screen_name = self.username,count=200,max_id=oldest)
+    
+                #save most recent tweets
+                self.alltweets.extend(new_tweets)
+    
+                #update the id of the oldest tweet less one
+                oldest = self.alltweets[-1].id - 1
+                print ("...%s tweets downloaded so far" % (len(self.alltweets)))
+    
+            #transform the tweepy tweets into a 2D array that will populate the csv
+            outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in self.alltweets]
+            location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+            if(createFile):
+                with open(os.path.join(location, '%s_tweets.csv' % self.username), 'w+') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(["id","created_at","text"])
+                    writer.writerows(outtweets)
+            
+            if(self.filtered == False):
+                for tweet in self.alltweets:
+                    self.semanticScores[tweet.created_at] = TextBlob(tweet.text).sentiment
+            else:
+                for tweet in self.alltweets:
+                    tweetByWords = tweet.text.split()
+                    
+                    
+                    for word in tweetByWords:
+                        for filterWord in self.filterWords:
+                            if(word == filterWord):
+                                self.semanticScores[tweet.created_at] = TextBlob(tweet.text).sentiment
+                                break
+                            else:
+                                continue
                             break
-                        else:
-                            continue
-                        break
-                    else:
-                        continue
-                    break
-
 
 
     def getSentimentScores(self):
@@ -118,13 +129,17 @@ if __name__ == "__main__":
         scores = a.getSentimentScores()
 
         for date in scores:
-
-            weekString = str(date.year)
-
-            if date.isocalendar()[1] < 10 :
+            if type(date) is str:
+                datetimeObj = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+            else:
+                datetimeObj = date
+                
+            weekString = str(datetimeObj.year)
+            
+            if datetimeObj.isocalendar()[1] < 10 :
                 weekString += '0'
-
-            weekString+= str(date.isocalendar()[1])
+            
+            weekString+= str(datetimeObj.isocalendar()[1])
 
             if(weekString in values):
                 values[weekString] += scores[date][0]
